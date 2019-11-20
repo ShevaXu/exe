9396e076088f311b6aaabecf9596a11f9aed123e
Sheva Xu <shevaxu@outlook.com>
2019-11-20 20:13:06 +0800

    test: add cases

--- cmd_test.go
+++ cmd_test.go
@@ -2,6 +2,9 @@ package exe_test
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/practigo/exe"
@@ -15,4 +18,26 @@ func TestCmd(t *testing.T) {
 
 	// cannot recognize pipe `|`
 	t.Log(exe.Cmd("ps -ef | grep go").Exec(context.TODO(), exe.Pre(exe.Pipe)))
+}
+
+func TestStd(t *testing.T) {
+	logF, err := ioutil.TempFile("", "log")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = exe.Std.Run(context.TODO(), "ls", logF.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Remove(logF.Name())
+
+	stdout, _, err := exe.Std.Probe(context.TODO(), "pwd")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pwd := strings.TrimSpace(string(stdout))
+	wd, _ := os.Getwd()
+	if pwd != wd {
+		t.Error(pwd, "!=", wd)
+	}
 }
